f73b0a3256d0f4e5a83dfdb650bb7ccb7c8a21df
joshasgard <idowuodesanmi@gmail.com>
2022-01-04 07:15:50 +0100

    Add models for admin to manage portfolio projects

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+# Shows Project model in admin page
+admin.site.register(Project)
--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Project(models.Model):
+    # Creates Model class, called Project here, for inputting portolio projects as objects. 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to = 'projects/images/')
