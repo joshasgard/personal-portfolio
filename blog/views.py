ed90aa6142e3709c113ae51a4a954d341102ec17
joshasgard <idowuodesanmi@gmail.com>
2022-01-05 11:33:47 +0100

    CREATE BLOG CONTENT PAGE AND ADD STATIC MEDIA TO HOME PAGE
    Commit creates a new blog description page for viewing blog content and a link to author's resume and his image was added to the homepage in projects/static

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Myblogs
 
 
 def all_blogs(request):
-    blogposts = Myblogs.objects.all()
+    # Show all blogs all limit the number with python list indexing
+    blogposts = Myblogs.objects.order_by('-date')[:3]
 
     return render(request, 'blog/all_blogs.html',{'myblogs':blogposts})
 
+def detail(request, blog_id):
+    blog = get_object_or_404(Myblogs, pk=blog_id)
+    return render(request, 'blog/details.html',{'blog':blog})
